423239db636a3ac8fe4c257a0c981cd823d106e1
KB Kim <kyungbeenkim@yonsei.ac.kr>
2024-10-30 14:27:05 +0900

    start.sh (changed)

--- app/start.sh
+++ app/start.sh
@@ -1,46 +1,5 @@
-#!/bin/sh
-
-echo ""
-# echo 'Creating python virtual environment ".venv"'
-echo ""
-cd ./backend
-# python3 -m venv .venv
-python app.py
-
-echo ""
-echo "Restoring backend python packages"
-echo ""
-./.venv/bin/python -m pip install -r requirements.txt
-if [ $? -ne 0 ]; then
-    echo "Failed to restore backend python packages"
-    exit $?
-fi
-
-echo ""
-echo "Restoring frontend npm packages"
-echo ""
-cd ../frontend
-npm install
-if [ $? -ne 0 ]; then
-    echo "Failed to restore frontend npm packages"
-    exit $?
-fi
-
-echo ""
-echo "Building frontend"
-echo ""
-npm run build
-if [ $? -ne 0 ]; then
-    echo "Failed to build frontend"
-    exit $?
-fi
-
 echo ""
 echo "Starting backend"
 echo ""
-cd ../backend
-./.venv/bin/python app.py --reload
-if [ $? -ne 0 ]; then
-    echo "Failed to start backend"
-    exit $?
-fi
+cd ./backend
+python app.py
